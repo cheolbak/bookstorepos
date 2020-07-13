70da47547f43c9a1453cf5dccff056fb0755231d
git_3206 <gudrjsdn8825@gmail.com>
2020-07-13 18:27:21 +0900

    pos_Customer TABLE, Customer 에 point 값 추가

--- src/main/java/kr/re/kitri/fiveminutes/bookstorepos/domain/Customer.java
+++ src/main/java/kr/re/kitri/fiveminutes/bookstorepos/domain/Customer.java
@@ -1,15 +1,20 @@
 package kr.re.kitri.fiveminutes.bookstorepos.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
 
     private int customerId;
     private String customerName;
     private String customerTel;
+    private int customerPoint;
     private LocalDateTime createDate;
     private LocalDateTime modifyDate;
 
