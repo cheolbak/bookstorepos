7e18b5704555d603073124dc9846415cd81aaa69
Mingyu Kwak <cheolbak@outlook.com>
2020-07-20 17:04:29 +0900

    Customer Like로 찾기 변경

--- src/main/java/kr/re/kitri/fiveminutes/bookstorepos/dao/CustomerDAO.java
+++ src/main/java/kr/re/kitri/fiveminutes/bookstorepos/dao/CustomerDAO.java
@@ -1,17 +1,18 @@
 package kr.re.kitri.fiveminutes.bookstorepos.dao;
-import kr.re.kitri.fiveminutes.bookstorepos.domain.Book;
+
 import kr.re.kitri.fiveminutes.bookstorepos.domain.Customer;
 import kr.re.kitri.fiveminutes.bookstorepos.util.db.DBPlug;
 import lombok.extern.slf4j.Slf4j;
-import org.mindrot.jbcrypt.BCrypt;
 
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 @Slf4j
 public class CustomerDAO {
-    public Customer selectId(int id){
-        try(DBPlug dbPlug = new DBPlug()){
+    public Customer selectId(int id) {
+        try (DBPlug dbPlug = new DBPlug()) {
             return dbPlug.getMappedObjectFromExecuteQuery("customer.select_id",
                     new DBPlug.InjectPreparedStatement() {
                         @Override
@@ -32,22 +33,21 @@ public class CustomerDAO {
                             return customer;
                         }
                     });
-        }
-        catch (SQLException e){
-            if(log.isDebugEnabled()){
+        } catch (SQLException e) {
+            if (log.isDebugEnabled()) {
                 e.printStackTrace();
             }
         }
-        return new Customer(0,"ERROR","ERROR",0,0,LocalDateTime.now(),LocalDateTime.now());
+        return new Customer(0, "ERROR", "ERROR", 0, 0, LocalDateTime.now(), LocalDateTime.now());
     }
 
-    public Customer selectName(String name){
-        try(DBPlug dbPlug = new DBPlug()){
+    public Customer selectNameQuery(String query) {
+        try (DBPlug dbPlug = new DBPlug()) {
             return dbPlug.getMappedObjectFromExecuteQuery("customer.select_name",
                     new DBPlug.InjectPreparedStatement() {
                         @Override
                         public void inject(PreparedStatement pstmt) throws SQLException {
-                            pstmt.setString(1, name);
+                            pstmt.setString(1, "%" + query + "%");
                         }
                     },
                     new DBPlug.MappingResultSet<Customer>() {
@@ -63,22 +63,21 @@ public class CustomerDAO {
                             return customer;
                         }
                     });
-        }
-        catch (SQLException e){
-            if(log.isDebugEnabled()){
+        } catch (SQLException e) {
+            if (log.isDebugEnabled()) {
                 e.printStackTrace();
             }
         }
-        return new Customer(0,"ERROR","ERROR",0,0,LocalDateTime.now(),LocalDateTime.now());
+        return new Customer(0, "ERROR", "ERROR", 0, 0, LocalDateTime.now(), LocalDateTime.now());
     }
 
-    public Customer selectTel(String tel){
-        try(DBPlug dbPlug = new DBPlug()){
+    public Customer selectTelQuery(String query) {
+        try (DBPlug dbPlug = new DBPlug()) {
             return dbPlug.getMappedObjectFromExecuteQuery("customer.select_tel",
                     new DBPlug.InjectPreparedStatement() {
                         @Override
                         public void inject(PreparedStatement pstmt) throws SQLException {
-                            pstmt.setString(1, tel);
+                            pstmt.setString(1, "%" + query + "%");
                         }
                     },
                     new DBPlug.MappingResultSet<Customer>() {
@@ -94,13 +93,12 @@ public class CustomerDAO {
                             return customer;
                         }
                     });
-        }
-        catch (SQLException e){
-            if(log.isDebugEnabled()){
+        } catch (SQLException e) {
+            if (log.isDebugEnabled()) {
                 e.printStackTrace();
             }
         }
-        return new Customer(0,"ERROR","ERROR",0,0,LocalDateTime.now(),LocalDateTime.now());
+        return new Customer(0, "ERROR", "ERROR", 0, 0, LocalDateTime.now(), LocalDateTime.now());
     }
 
     public int insertCustomer(Customer customer) {
@@ -114,8 +112,7 @@ public class CustomerDAO {
                             pstmt.setString(3, customer.getCustomerTel());
                         }
                     });
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             if (log.isDebugEnabled()) {
                 e.printStackTrace();
             }
@@ -133,8 +130,7 @@ public class CustomerDAO {
                             pstmt.setInt(2, id);
                         }
                     });
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             if (log.isDebugEnabled()) {
                 e.printStackTrace();
             }
@@ -142,8 +138,8 @@ public class CustomerDAO {
         return 0;
     }
 
-    public int updateTel(int id, String tel){
-        try(DBPlug dbPlug = new DBPlug()){
+    public int updateTel(int id, String tel) {
+        try (DBPlug dbPlug = new DBPlug()) {
             return dbPlug.executeUpdateFromQuery("customer.update_tel",
                     new DBPlug.InjectPreparedStatement() {
                         @Override
@@ -152,17 +148,16 @@ public class CustomerDAO {
                             pstmt.setInt(2, id);
                         }
                     });
-        }
-        catch (SQLException e){
-            if(log.isDebugEnabled()){
+        } catch (SQLException e) {
+            if (log.isDebugEnabled()) {
                 e.printStackTrace();
             }
         }
         return 0;
     }
 
-    public int updateTotal(int id, int price){
-        try(DBPlug dbPlug = new DBPlug()){
+    public int updateTotal(int id, int price) {
+        try (DBPlug dbPlug = new DBPlug()) {
             return dbPlug.executeUpdateFromQuery("customer.update_total",
                     new DBPlug.InjectPreparedStatement() {
                         @Override
@@ -171,17 +166,16 @@ public class CustomerDAO {
                             pstmt.setInt(2, id);
                         }
                     });
-        }
-        catch (SQLException e){
-            if(log.isDebugEnabled()){
+        } catch (SQLException e) {
+            if (log.isDebugEnabled()) {
                 e.printStackTrace();
             }
         }
         return 0;
     }
 
-    public int deleteCustomer(int id){
-        try(DBPlug dbPlug = new DBPlug()){
+    public int deleteCustomer(int id) {
+        try (DBPlug dbPlug = new DBPlug()) {
             return dbPlug.executeUpdateFromQuery("customer.delete",
                     new DBPlug.InjectPreparedStatement() {
                         @Override
@@ -189,9 +183,8 @@ public class CustomerDAO {
                             pstmt.setInt(1, id);
                         }
                     });
-        }
-        catch (SQLException e){
-            if(log.isDebugEnabled()){
+        } catch (SQLException e) {
+            if (log.isDebugEnabled()) {
                 e.printStackTrace();
             }
         }
