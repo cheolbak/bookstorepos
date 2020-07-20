69b855006287d2976922f6bfe6498d101c81235d
Mingyu Kwak <cheolbak@outlook.com>
2020-07-20 09:16:49 +0900

    StockBookSearchPanel 뼈대 추가

--- src/main/java/kr/re/kitri/fiveminutes/bookstorepos/view/component/StockBookSearchPanel.java
+++ src/main/java/kr/re/kitri/fiveminutes/bookstorepos/view/component/StockBookSearchPanel.java
@@ -1,4 +1,51 @@
 package kr.re.kitri.fiveminutes.bookstorepos.view.component;
 
-public class StockBookSearchPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class StockBookSearchPanel extends JPanel {
+
+    public StockBookSearchPanel() {
+        super(new GridBagLayout());
+    }
+
+    private JTextField createISBNField() {
+        JTextField textField = new JTextField();
+        textField.addActionListener(e -> {
+
+        });
+        return textField;
+    }
+
+    private JButton createAddButton() {
+        JButton button = new JButton("추가");
+        button.addActionListener(e -> {
+
+        });
+        return button;
+    }
+
+    private JButton createReadBarcodeButton() {
+        JButton button = new JButton("추가");
+        button.addActionListener(e -> {
+
+        });
+        return button;
+    }
+
+    private JButton createSearchBookButton() {
+        JButton button = new JButton("추가");
+        button.addActionListener(e -> {
+
+        });
+        return button;
+    }
+
+    private JButton createShowNewBookButton() {
+        JButton button = new JButton("추가");
+        button.addActionListener(e -> {
+
+        });
+        return button;
+    }
 }
