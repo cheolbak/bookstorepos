25471384e246371ff744576122303a560f1a70a5
git_3206 <gudrjsdn8825@gmail.com>
2020-07-21 13:35:39 +0900

    SellDAO selectSell 추가 (+query)

--- src/main/java/kr/re/kitri/fiveminutes/bookstorepos/domain/Sell.java
+++ src/main/java/kr/re/kitri/fiveminutes/bookstorepos/domain/Sell.java
@@ -1,10 +1,14 @@
 package kr.re.kitri.fiveminutes.bookstorepos.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Sell {
 
     private int sellId;
