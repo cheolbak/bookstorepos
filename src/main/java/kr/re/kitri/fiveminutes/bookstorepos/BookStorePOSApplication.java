a7f3f5831b6b040f240bdf00bbdd1f404cf2c06d
git_3206 <gudrjsdn8825@gmail.com>
2020-07-21 16:28:01 +0900

    merge into Branch

--- src/main/java/kr/re/kitri/fiveminutes/bookstorepos/BookStorePOSApplication.java
+++ src/main/java/kr/re/kitri/fiveminutes/bookstorepos/BookStorePOSApplication.java
@@ -8,7 +8,6 @@ import javax.swing.*;
 
 public class BookStorePOSApplication {
     public static void main(String[] args) {
-        //SwingUtilities.invokeLater(TFrame::new);
         //SwingUtilities.invokeLater(AuthDialogFrame::new);
         SwingUtilities.invokeLater(MainFrame::new);
     }
