d1a106d138aed614aa0893efcffe926dcc50ab2a
Mingyu Kwak <cheolbak@outlook.com>
2020-07-23 13:36:52 +0900

    로그인 비밀번호 설정

--- src/main/java/kr/re/kitri/fiveminutes/bookstorepos/BookStorePOSApplication.java
+++ src/main/java/kr/re/kitri/fiveminutes/bookstorepos/BookStorePOSApplication.java
@@ -1,14 +1,11 @@
 package kr.re.kitri.fiveminutes.bookstorepos;
 
-import kr.re.kitri.fiveminutes.bookstorepos.view.MainFrame;
 import kr.re.kitri.fiveminutes.bookstorepos.view.module.AuthDialogFrame;
-import kr.re.kitri.fiveminutes.bookstorepos.view.module.BookSearchDialogFrame;
 
 import javax.swing.*;
 
 public class BookStorePOSApplication {
     public static void main(String[] args) {
-        // SwingUtilities.invokeLater(AuthDialogFrame::new);
-        SwingUtilities.invokeLater(MainFrame::new);
+        SwingUtilities.invokeLater(AuthDialogFrame::new);
     }
 }
